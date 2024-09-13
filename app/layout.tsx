54ea9914ab5cc9bb3a3d5d2b5216ef4f020e2be1
Kundan Kumar Sahu <espkundan@gmail.com>
2024-09-13 23:01:03 +0530

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { newAgeFont } from "@/lib/fonts/font";
-import { DM_Sans } from "next/font/google";
-import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
+import type { Metadata } from 'next';
+import './globals.css';
+import { newAgeFont } from '@/lib/fonts/font';
+import { DM_Sans } from 'next/font/google';
+import './globals.css';
+import { Toaster } from '@/components/ui/sonner';
 import { Analytics } from '@vercel/analytics/react';
 
-const font = DM_Sans({ subsets: ["latin"] });
+const font = DM_Sans({ subsets: ['latin'] });
 export const metadata: Metadata = {
-  title: "sanbox 23",
-  description: "One platform for all your projects",
+  title: 'sanbox 23',
+  description: 'One platform for all your projects',
 };
 
 // components
-import { HeaderComponent } from "@/components/NavBar";
+import { HeaderComponent } from '@/components/NavBar';
 
 export default function RootLayout({
   children,
@@ -22,11 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html
-    suppressHydrationWarning={true}
+      suppressHydrationWarning={true}
       lang="en"
       className={` ${newAgeFont.className} bg-white overflow-x-hidden   h-full text-black  flex flex-col w-screen px-5 sm:px-7 md:px-10 lg:px-12 xl:px-16 xl:pl-12 `}
       style={{
-        backgroundColor: "#ffffff",
+        backgroundColor: '#ffffff',
       }}
     >
       <body className="bg-white">
