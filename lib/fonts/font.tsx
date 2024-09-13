70bd53d8dc6f971855cbfe80ccf577d31abc77fc
hemanth <applicationcomputer257@gmail.com>
2024-09-13 13:15:04 +0530

    add nav , publick files , fonts

---
+++ lib/fonts/font.tsx
@@ -0,0 +1,15 @@
+
+
+import localFont from "next/font/local";
+
+export const newAgeFont=localFont({
+    src:"../../lib/fonts/a.woff2",
+    display:"swap",
+    weight:"600",
+})
+
+export const newAgeFontBold=localFont({
+    src:"../../lib/fonts/a.woff2",
+    display:"swap",
+    weight:"800",
+})
